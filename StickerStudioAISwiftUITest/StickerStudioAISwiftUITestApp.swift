2667b02b98b15538bb346dc42bdfb3ab54c2d454
Ivan Lebedev <ivan.lebedev.developer@gmail.com>
2023-12-05 22:57:02 +0500

    Initial Commit

---
+++ StickerStudioAISwiftUITest/StickerStudioAISwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerStudioAISwiftUITestApp.swift
+//  StickerStudioAISwiftUITest
+//
+//  Created by Ivan Lebedev on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StickerStudioAISwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
